36593c65fd2a214323e87c820925a790c4c37b78
Anjfdgbkjfdb <st21373@ormiston.school.nz>
2021-09-22 15:47:59 +1200

    Base v1
    Allows user to enter data but does not save it

--- Assessment.py
+++ Assessment.py
@@ -1,11 +1,64 @@
 from tkinter import *
 root = Tk()
 
-root.geometry("400x200")
+root.geometry("400x400")
 def quit():
     root.destroy()
 
 b1 = Button (root, text="Quit", command=quit)
 b1.pack() 
+
+def enter():
+    leader=e1.get() 
+    string="Name = " + leader
+    l1["text"]=string
+
+    plan=e2.get()
+    string="Receipt number = " + plan
+    l2["text"]=string
+
+    campers=e3.get()
+    string="Item hired = " + campers
+    l3["text"]=string
+
+    weather=e4.get()
+    string="Quantity = " + weather
+    l4["text"]=string
+
+b1 = Button (root, text="Enter", command=enter)
+la = Label(root, text="Name: ")
+lb = Label(root, text="Receipt number: ")
+lc = Label(root, text="Item hired: ")
+ld = Label(root, text="Quantity: ")
+
+
+e1=Entry(root)
+l1=Label(root)
+
+e2=Entry(root)
+l2=Label(root)
+
+e3=Entry(root)
+l3=Label(root)
+
+e4=Entry(root)
+l4=Label(root)
+
+b1.pack()
+e1.pack() 
+l1.pack()
+la.pack()
+
+e2.pack()
+l2.pack()
+lb.pack()
+
+e3.pack()
+l3.pack()
+lc.pack()
+
+e4.pack()
+l4.pack()
+ld.pack()
 
 root.mainloop()
