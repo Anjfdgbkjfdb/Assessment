530a5d8a2f1e15b35e2c0cfd9b67c5988a3288ee
Anjfdgbkjfdb <st21373@ormiston.school.nz>
2021-09-25 17:38:22 +1200

    Base code v2
    Code is able to now save and print details when button is pressed

--- Assessment.py
+++ Assessment.py
@@ -1,6 +1,9 @@
 from tkinter import *
 root = Tk()
 
+receipt_detail = []
+total_entry = 0
+
 root.geometry("400x400")
 def quit():
     root.destroy()
@@ -8,57 +11,31 @@ def quit():
 b1 = Button (root, text="Quit", command=quit)
 b1.pack() 
 
-def enter():
-    Name=e1.get() 
-    string="Name = " + Name
-    l1["text"]=string
+def save():
+    global receipt_detail,Name,Receipt,Item,Qua,total_entry
 
-    Receipt=e2.get()
-    string="Receipt number = " + Receipt
-    l2["text"]=string
+    receipt_detail.append([Name.get(),Receipt.get(),Item.get(),Qua.get()]) 
+    total_entry +=  1
 
-    Item=e3.get()
-    string="Item hired = " + Item
-    l3["text"]=string
+def print():
+    global total_entry, receipt_detail,Name,Receipt,Item,Qua
+    string= receipt_detail
+    Label(root,text = string).pack()
 
-    Qua=e4.get()
-    string="Quantity = " + Qua
-    l4["text"]=string
+Button (root, text="Append Details", command=save).pack()
+Label(root, text="Name: ").pack()
+Label(root, text="Receipt number: ").pack()
+Label(root, text="Item hired: ").pack()
+Label(root, text="Quantity: ").pack()
+Button(root, text="Print Details",command=print,width = 10) .pack()
 
-b1 = Button (root, text="Enter", command=enter)
-la = Label(root, text="Name: ")
-lb = Label(root, text="Receipt number: ")
-lc = Label(root, text="Item hired: ")
-ld = Label(root, text="Quantity: ")
-
-
-e1=Entry(root)
-l1=Label(root)
-
-e2=Entry(root)
-l2=Label(root)
-
-e3=Entry(root)
-l3=Label(root)
-
-e4=Entry(root)
-l4=Label(root)
-
-b1.pack()
-e1.pack() 
-l1.pack()
-la.pack()
-
-e2.pack()
-l2.pack()
-lb.pack()
-
-e3.pack()
-l3.pack()
-lc.pack()
-
-e4.pack()
-l4.pack()
-ld.pack()
+Name=Entry(root)
+Name.pack()
+Receipt=Entry(root)
+Receipt.pack()
+Item=Entry(root)
+Item.pack()
+Qua=Entry(root)
+Qua.pack()
 
 root.mainloop()
