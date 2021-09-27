4e5f1814477cdea6eba51028bfbc0e3338abafba
Anjfdgbkjfdb <st21373@ormiston.school.nz>
2021-09-27 22:28:01 +1300

    Base code v2 (positioned)
    Positioned each element of the GUI using .grid

--- Assessment.py
+++ Assessment.py
@@ -5,11 +5,9 @@ receipt_detail = []
 total_entry = 0
 
 root.geometry("400x400")
+
 def quit():
     root.destroy()
-
-b1 = Button (root, text="Quit", command=quit)
-b1.pack() 
 
 def save():
     global receipt_detail,Name,Receipt,Item,Qua,total_entry
@@ -18,24 +16,33 @@ def save():
     total_entry +=  1
 
 def print():
-    global total_entry, receipt_detail,Name,Receipt,Item,Qua
-    string= receipt_detail
-    Label(root,text = string).pack()
+    global total_entry, row_num
+    row_num = 0
+    while row_num < total_entry :
+        Label(root, text=row_num).grid(column=1,row=row_num+8) 
+        Label(root, text=(receipt_detail[row_num][0])).grid(column=2,row=row_num+8)
+        Label(root, text=(receipt_detail[row_num][1])).grid(column=3,row=row_num+8)
+        Label(root, text=(receipt_detail[row_num][2])).grid(column=4,row=row_num+8)
+        Label(root, text=(receipt_detail[row_num][3])).grid(column=5,row=row_num+8)
+        row_num +=  1
 
-Button (root, text="Append Details", command=save).pack()
-Label(root, text="Name: ").pack()
-Label(root, text="Receipt number: ").pack()
-Label(root, text="Item hired: ").pack()
-Label(root, text="Quantity: ").pack()
-Button(root, text="Print Details",command=print,width = 10) .pack()
+Button (root, text="Quit", command=quit).grid(column=5, row=2)
+Button (root, text="Append Details", command=save).grid(column=5, row=4)
+Button(root, text="Print Details",command=print,width = 10).grid(column=5,row=5)
+Label(root, text="        ").grid(column=1,row=1)
+Label(root, text="        ").grid(column=4,row=2)
 
+Label(root, text="Name: ").grid(column=2,row=2)
 Name=Entry(root)
-Name.pack()
+Name.grid(column=3, row=2)
+Label(root, text="Receipt number: ").grid(column=2,row=3)
 Receipt=Entry(root)
-Receipt.pack()
+Receipt.grid(column=3,row=3)
+Label(root, text="Item hired: ").grid(column=2,row=4)
 Item=Entry(root)
-Item.pack()
+Item.grid(column=3,row=4)
+Label(root, text="Quantity: ").grid(column=2,row=5)
 Qua=Entry(root)
-Qua.pack()
+Qua.grid(column=3,row=5)
 
 root.mainloop()
